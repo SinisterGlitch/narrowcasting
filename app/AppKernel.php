4fea849d6061a008f4c207a88a7df703c4146426
Germain Riahi <germain.riahi@sqill.com>
2015-06-19 23:15:09 +0200

    Added rest configuration and setup UserController

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new Bestcasting\Manage\CoreBundle\ManageCoreBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
