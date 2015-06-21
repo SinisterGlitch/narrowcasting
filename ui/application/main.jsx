18d2e237df572864454d5d54ccf1d3a59b3a65e6
Germain Riahi <germain.riahi@sqill.com>
2015-06-21 17:19:28 +0200

    Creating folder structure and including bootstrap

--- ui/application/main.jsx
+++ ui/application/main.jsx
@@ -1,18 +1,15 @@
 var _ = require('lodash');
 var React = require('react');
 var ReactRouter = require('react-router');
-var Bootstrapper = require('bootstrapper');
 var Reflux = require('reflux');
 
 module.exports = function() {
     _.assign(Reflux.ActionMethods, require('core/reflux/action-methods'));
 };
 
-// Set up application
 var App = require('core/components/layout/app');
 var Routes = require('routes');
 
-// Run the react router
 ReactRouter.run(Routes, function (Handler) {
     React.render(<Handler/>, document.getElementById('container'));
 });
